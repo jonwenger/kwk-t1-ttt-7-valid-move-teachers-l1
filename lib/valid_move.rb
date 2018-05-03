d855bdd0add5b8b68e0f03d7a64d9bc635c4f4e5
Jon Wenger <jonwenger783@gmail.com>
2018-05-03 15:59:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,10 @@
 # code your #valid_move? method here
+board = ["x","x","x","x","x","x","x","x"]
+index=2
 def valid_move?(board, index)
 num=index-1
 
-  
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
