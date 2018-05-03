cb128f00f3bccffd475f102b9d786ae60dee923e
Jon Wenger <jonwenger783@gmail.com>
2018-05-03 15:57:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  num = index-1
-  open_spot == position_taken?(board,num)
-  puts  "#{open_spot}"
+true
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
